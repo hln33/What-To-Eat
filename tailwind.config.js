7286ca464755bd747b021e04b1b8b49355782a50
Harry Nguyen <hln5@sfu.ca>
2025-01-21 15:12:24 -0800

    dep: add kobalte plugin for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
+import kobaltePlugin from '@kobalte/tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-  ],  theme: {
-    extend: {},
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        contentShow: {
+          'from': { opacity: '0' },
+          'to': { opacity: '1' },
+        },
+        contentHide: {
+          'from': { opacity: '1' },
+          'to': { opacity: '0' },
+        }
+      },
+      animation: {
+        'content-show': 'contentShow 250ms ease-out',
+        'content-hide': 'contentHide 250ms ease-in forwards'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    kobaltePlugin
+  ],
 }
 
