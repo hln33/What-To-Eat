82c48537fbc85a44921f5230a7d03421c05e63e7
Harry Nguyen <hln5@sfu.ca>
2025-01-21 15:20:34 -0800

    dep: init prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { type Component } from 'solid-js';
+import { type Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
     <div class="text-center">
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+        <img
+          src={logo}
+          class={styles.logo}
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
