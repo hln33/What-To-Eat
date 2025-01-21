5b98e0c03048414ecf314816a9c3ce51689d5ef0
Harry Nguyen <hln5@sfu.ca>
2025-01-21 15:36:16 -0800

    dep: add dep for tailwind and postcss

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,11 @@ const App: Component = () => {
   return (
     <div class="text-center">
       <header class={styles.header}>
-        <img
-          src={logo}
-          class={styles.logo}
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <h1>What to Eat?</h1>
       </header>
+      <main>
+        <h1>Essentials</h1>
+      </main>
     </div>
   );
 };
