b84f08ff3643f3f3e6a2f7b5a6ff40fc8fc08856
Harry Nguyen <hln5@sfu.ca>
2025-01-23 12:06:56 -0800

    feat: create template page for recipe

--- src/app/pages/Recipe.tsx
+++ src/app/pages/Recipe.tsx
@@ -8,35 +8,35 @@ const RecipePage: Component = () => {
 
         <section>
           <h3 class="text-left text-3xl">Ingredients</h3>
-          <ul class="list-inside list-disc">
-            <li class="text-left">3 Eggs</li>
-            <li class="text-left">Olive Oil / Butter</li>
+          <ul class="list-inside list-disc text-left">
+            <li>3 Eggs</li>
+            <li>Olive Oil / Butter</li>
           </ul>
         </section>
 
         <section class="space-y-2">
           <h3 class="text-left text-3xl">Instructions</h3>
-          <ul class="list-outside list-decimal space-y-2">
-            <li class="text-left">
+          <ul class="list-outside list-decimal space-y-2 text-left">
+            <li>
               Crack the eggs into a medium bowl. Whisk until smooth and
               combined, with no streaks of egg white remaining.
             </li>
-            <li class="text-left">
+            <li>
               Brush a small nonstick skillet with olive oil, or melt a little
               butter in a small nonstick skillet. Bring to medium heat.
             </li>
-            <li class="text-left">
+            <li>
               Pour in the eggs, and let them cook for a few seconds without
               stirring. Pull a rubber spatula across the bottom of the pan to
               form large, soft curds of scrambled eggs.
             </li>
-            <li class="text-left">
+            <li>
               Continue cooking over medium-low heat, folding and stirring the
               eggs every few seconds. Scrape the spatula along the bottom and
               sides of the pan often to form more curds and to prevent any part
               of the eggs from drying out.
             </li>
-            <li class="text-left">
+            <li>
               Remove the pan from the heat when the eggs are mostly set, but a
               little liquid egg remains. Season to taste with salt and pepper.
             </li>
