fa80e9dfea546bd01e763ef308dd9779d450e544
Harry Nguyen <hln5@sfu.ca>
2025-01-23 15:52:19 -0800

    feat(server): set up route for recipes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,16 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import recipes from './recipes.ts';
 
 const app = new Hono();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
+app.route('/recipes', recipes);
 
 const port = 3001;
 console.log(`Server is running on http://localhost:${port}`);
-
 serve({
   fetch: app.fetch,
   port,
