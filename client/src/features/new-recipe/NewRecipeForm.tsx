cc62383e04a6d9ab510818c263b5a26e6f639f30
Harry Nguyen <hln5@sfu.ca>
2025-02-24 12:28:12 -0800

    Add title for form

--- client/src/features/new-recipe/NewRecipeForm.tsx
+++ client/src/features/new-recipe/NewRecipeForm.tsx
@@ -27,80 +27,84 @@ const NewRecipeForm = () => {
   };
 
   return (
-    <Form
-      class="space-y-8"
-      onSubmit={handleSubmit}
-    >
-      <Field name="recipeName">
-        {(field, props) => (
-          <TextField
-            {...props}
-            type="text"
-            label="Recipe Name"
-            value={field.value}
-            error={field.error}
-            required
-          />
-        )}
-      </Field>
+    <div class="space-y-10">
+      <h2 class="text-3xl">New Recipe</h2>
 
-      <For each={["ingredients", "steps"] as const}>
-        {(fieldName, _) => (
-          <div class="space-y-2">
-            <FieldArray name={fieldName}>
-              {(fieldArray) => (
-                <>
-                  <label
-                    class="block text-left capitalize"
-                    for={fieldArray.name}
-                  >
-                    {fieldName} *
-                  </label>
-                  <For each={fieldArray.items}>
-                    {(_, index) => (
-                      <div class="mb-2 mt-1 flex">
-                        <Field name={`ingredients.${index()}`}>
-                          {(field, props) => (
-                            <TextField
-                              {...props}
-                              type="text"
-                              value={field.value}
-                              error={field.error}
-                              required
-                            />
-                          )}
-                        </Field>
-                        <Button
-                          class="size-12"
-                          onClick={() =>
-                            remove(form, fieldArray.name, { at: index() })
-                          }
-                        >
-                          X
-                        </Button>
-                      </div>
-                    )}
-                  </For>
-                </>
-              )}
-            </FieldArray>
-            <Button
-              class="block w-fit px-4 capitalize"
-              onClick={() => insert(form, fieldName, { value: "" })}
-            >
-              + {fieldName}
-            </Button>
-          </div>
-        )}
-      </For>
-
-      <Button
-        class="w-full"
-        type="submit"
+      <Form
+        class="space-y-8"
+        onSubmit={handleSubmit}
       >
-        Submit
-      </Button>
-    </Form>
+        <Field name="recipeName">
+          {(field, props) => (
+            <TextField
+              {...props}
+              type="text"
+              label="Recipe Name"
+              value={field.value}
+              error={field.error}
+              required
+            />
+          )}
+        </Field>
+
+        <For each={["ingredients", "steps"] as const}>
+          {(fieldName, _) => (
+            <div class="space-y-2">
+              <FieldArray name={fieldName}>
+                {(fieldArray) => (
+                  <>
+                    <label
+                      class="block text-left capitalize"
+                      for={fieldArray.name}
+                    >
+                      {fieldName} *
+                    </label>
+                    <For each={fieldArray.items}>
+                      {(_, index) => (
+                        <div class="mb-2 mt-1 flex">
+                          <Field name={`ingredients.${index()}`}>
+                            {(field, props) => (
+                              <TextField
+                                {...props}
+                                type="text"
+                                value={field.value}
+                                error={field.error}
+                                required
+                              />
+                            )}
+                          </Field>
+                          <Button
+                            class="size-12"
+                            onClick={() =>
+                              remove(form, fieldArray.name, { at: index() })
+                            }
+                          >
+                            X
+                          </Button>
+                        </div>
+                      )}
+                    </For>
+                  </>
+                )}
+              </FieldArray>
+              <Button
+                class="block w-fit px-4 capitalize"
+                onClick={() => insert(form, fieldName, { value: "" })}
+              >
+                + {fieldName}
+              </Button>
+            </div>
+          )}
+        </For>
+
+        <Button
+          class="w-full"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 };
 
