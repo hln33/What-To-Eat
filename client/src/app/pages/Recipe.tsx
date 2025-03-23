c33659e882aaa4d21fb3371a95051c541edb97b1
Harry Nguyen <hln5@sfu.ca>
2025-03-22 20:42:23 -0700

    pre-commit: run tests headlessly

--- client/src/app/pages/Recipe.tsx
+++ client/src/app/pages/Recipe.tsx
@@ -1,10 +1,12 @@
-import { ErrorBoundary, Index, Suspense, type Component } from "solid-js";
+import { ErrorBoundary, For, Index, Suspense, type Component } from "solid-js";
 import { A, useParams } from "@solidjs/router";
 import { createQuery } from "@tanstack/solid-query";
 import { Separator } from "@kobalte/core/separator";
-import Skeleton from "@/components/ui/Skeleton";
+import StarIcon from "~icons/fe/star";
+
 import { getRecipe } from "@/features/recipes/api";
 import DeleteRecipeDialog from "@/features/recipes/components/DeleteRecipeDialog";
+import Skeleton from "@/components/ui/Skeleton";
 
 const RecipePage: Component = () => {
   const params = useParams();
@@ -14,16 +16,18 @@ const RecipePage: Component = () => {
     queryFn: () => getRecipe(params.id),
   }));
 
+  const MAX_RATING = 5;
+  const rating = 3;
+
   return (
     <div class="space-y-10">
-      <nav class="flex w-full justify-between">
+      <nav class="flex items-center justify-between">
         <A
           class="text-lg underline"
           href="/"
         >
           Go Back
         </A>
-
         <DeleteRecipeDialog recipeId={params.id} />
       </nav>
 
@@ -40,6 +44,17 @@ const RecipePage: Component = () => {
           <section class="space-y-12 text-left">
             <div class="space-y-5">
               <h2 class="text-5xl">{recipeQuery.data?.name}</h2>
+              <div
+                class="flex"
+                aria-label={`Rating: ${rating} out of ${MAX_RATING} stars`}
+              >
+                <For each={Array(rating)}>
+                  {() => <StarIcon>rating</StarIcon>}
+                </For>
+                <For each={Array(MAX_RATING - rating)}>
+                  {() => <StarIcon class="text-slate-500">rating</StarIcon>}
+                </For>
+              </div>
               <Separator />
             </div>
 
@@ -47,16 +62,16 @@ const RecipePage: Component = () => {
               <h3 class="text-3xl">Ingredients</h3>
               <ul class="list-inside list-disc">
                 <Index each={recipeQuery.data?.ingredients}>
-                  {(ingredient, _index) => <li>{ingredient()}</li>}
+                  {(ingredient) => <li>{ingredient()}</li>}
                 </Index>
               </ul>
             </section>
 
             <section class="space-y-3">
               <h3 class="text-3xl">Instructions</h3>
-              <ul class="list-outside list-decimal space-y-5">
+              <ul class="list-inside list-decimal space-y-5">
                 <Index each={recipeQuery.data?.instructions}>
-                  {(instruction, _index) => <li>{instruction()}</li>}
+                  {(instruction) => <li>{instruction()}</li>}
                 </Index>
               </ul>
             </section>
